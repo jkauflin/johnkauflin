7926daee684ebdd347782fa59067f799045df39c
John J Kauflin <johnkauflin@gmail.com>
2020-12-13 21:37:36 -0500

    Adding jjklogin

---
+++ jjkloginSettings.php
@@ -0,0 +1,20 @@
+<?php
+/*==============================================================================
+ * (C) Copyright 2020 John J Kauflin, All rights reserved. 
+ *----------------------------------------------------------------------------
+ * DESCRIPTION: An example setting file
+ *----------------------------------------------------------------------------
+ * Modification History
+ * 2020-12-13 JJK 	Initial version
+ *============================================================================*/
+
+// Set this to include a secrets file from an external (non public-html) file location
+require_once "../../external_includes/jjkloginSecrets.php";
+
+// The secrets file should include the following variables
+
+// When executing from within PHP files, add this to the front of URI paths to get to the parent directory
+// for under /vendor/<user>/<package>
+$phpRootReset = "../../../";
+
+?>
